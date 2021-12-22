514e72566994f7de2291484491c22467fe30209f
jamahadrummer <jamahadrummer@live.com>
2021-12-22 14:50:31 -0600

    js start

---
+++ app.js
@@ -0,0 +1,14 @@
+function computerPlay() {
+	let play = ['Rock', 'Paper', 'Scissors'];
+	let choice = random();
+	play.slice
+
+};
+
+console.log(computerPlay())
+console.log(computerPlay())
+console.log(computerPlay())
+console.log(computerPlay())
+console.log(computerPlay())
+console.log(computerPlay())
+console.log(computerPlay())
