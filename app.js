4df6373a6f2c92dc2bc7b63c3ed7f9f13f5637b4
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-07-07 15:53:40 -0500

    progress using youtube tutorial

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
-const myArray = ["Rock", "Paper", "Scissors"];
-
+const compArray = ["Rock", "Paper", "Scissors"];
 function computerPlay() {
-	return myArray[~~(Math.random() * myArray.length)];
+	return compArray[~~(Math.random() * compArray.length)];
+}
+// Show a confirm dialogue allowing user to consent to play.
+let playGame = confirm("Rock, Paper, or Scissors?");
+// If playGame is accepted prompt for a choice.
+if (playGame) {
+	// User fills playerChoice variable with their play
+	let playerChoice = prompt('Please enter Rock, Paper, or Scissors.');
+	if (playerChoice) {
+		// user trim function to change entry to lower case
+		let playerChose = playerChoice.trim().toLowerCase();
+		// check if playerChoice is any of the acceptable entries
+		console.log(playerChose)
+		if (playerChose === 'rock' || playerChose === 'paper' || playerChose === 'scissors') {
+			let computerChose = computerPlay();
+			console.log(computerChose);
+			if (computerChose === playerChose) {
+				alert("It's a tie! Reload and try again")
+			} else (computerChose === 'paper' && playerChose === 'rock'); {
+				alert("Computer wins, Paper covers rock. Reload and try again")
+			} 
+		} else {
+			alert('Incorrect Entry Please enter Rock, Paper, or Scissors')
+			alert('Reload the page and start again')
+		}
+	} else {
+		alert("Sorry you changed your mind.")
+	}
+} else {
+	alert("Ok, maybe next time.")
 }
 
 
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
+
+// // Personal Attempts
+// const compArray = ["Rock", "Paper", "Scissors"];
+
+// function userPlay() {
+// }
+
+// function computerPlay() {
+// 	return compArray[~~(Math.random() * compArray.length)];
+// }
+
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+
