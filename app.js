e725c722df43b3d18e16899a60480666d4dc1fd5
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-07-08 11:48:01 -0500

    game is operational

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const compArray = ["Rock", "Paper", "Scissors"];
+const compArray = ["rock", "paper", "scissors"];
 function computerPlay() {
 	return compArray[~~(Math.random() * compArray.length)];
 }
+
 // Show a confirm dialogue allowing user to consent to play.
 let playGame = confirm("Rock, Paper, or Scissors?");
 // If playGame is accepted prompt for a choice.
@@ -16,11 +17,26 @@ if (playGame) {
 		if (playerChose === 'rock' || playerChose === 'paper' || playerChose === 'scissors') {
 			let computerChose = computerPlay();
 			console.log(computerChose);
+			// Find Tie Condition
 			if (computerChose === playerChose) {
 				alert("It's a tie! Reload and try again")
-			} else (computerChose === 'paper' && playerChose === 'rock'); {
+				// Paper covers rock options
+			} else (computerChose != playerChose)
+			if (computerChose === 'paper' && playerChose === 'rock') {
 				alert("Computer wins, Paper covers rock. Reload and try again")
-			} 
+			} else if (computerChose === 'rock' && playerChose === 'paper') {
+				alert("User wins, Paper Covers Rock. Reload and try again")
+				// Rock Breaks scissors options
+			} else if (computerChose === 'rock' && playerChose === 'scissors') {
+				alert("Computer wins, Rock breaks Scissors. Reload and try again")
+			} else if (computerChose === 'scissors' && playerChose === 'rock') {
+				alert("User wins, Rock breaks Scissors. Reload and try again")
+				// Scissors cut Paper Options
+			} else if (computerChose === 'scissors' && playerChose === 'paper') {
+				alert("Computer wins, Scissors cut paper. Reload and try again")
+			} else if (computerChose === 'paper' && playerChose === 'scissors') {
+				alert("User wins, Scissors cut paper. Reload and try again")
+			}
 		} else {
 			alert('Incorrect Entry Please enter Rock, Paper, or Scissors')
 			alert('Reload the page and start again')
