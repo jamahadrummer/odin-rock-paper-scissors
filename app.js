945a6c21d77d833bc220c0b97a8d8aaa04638505
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-07-08 12:18:52 -0500

    playRound fuction added

--- app.js
+++ app.js
@@ -2,48 +2,55 @@ const compArray = ["rock", "paper", "scissors"];
 function computerPlay() {
 	return compArray[~~(Math.random() * compArray.length)];
 }
-
-// Show a confirm dialogue allowing user to consent to play.
-let playGame = confirm("Rock, Paper, or Scissors?");
-// If playGame is accepted prompt for a choice.
-if (playGame) {
-	// User fills playerChoice variable with their play
-	let playerChoice = prompt('Please enter Rock, Paper, or Scissors.');
-	if (playerChoice) {
-		// user trim function to change entry to lower case
-		let playerChose = playerChoice.trim().toLowerCase();
-		// check if playerChoice is any of the acceptable entries
-		console.log(playerChose)
-		if (playerChose === 'rock' || playerChose === 'paper' || playerChose === 'scissors') {
-			let computerChose = computerPlay();
-			console.log(computerChose);
-			// Find Tie Condition
-			if (computerChose === playerChose) {
-				alert("It's a tie! Reload and try again")
-				// Paper covers rock options
-			} else (computerChose != playerChose)
-			if (computerChose === 'paper' && playerChose === 'rock') {
-				alert("Computer wins, Paper covers rock. Reload and try again")
-			} else if (computerChose === 'rock' && playerChose === 'paper') {
-				alert("User wins, Paper Covers Rock. Reload and try again")
-				// Rock Breaks scissors options
-			} else if (computerChose === 'rock' && playerChose === 'scissors') {
-				alert("Computer wins, Rock breaks Scissors. Reload and try again")
-			} else if (computerChose === 'scissors' && playerChose === 'rock') {
-				alert("User wins, Rock breaks Scissors. Reload and try again")
-				// Scissors cut Paper Options
-			} else if (computerChose === 'scissors' && playerChose === 'paper') {
-				alert("Computer wins, Scissors cut paper. Reload and try again")
-			} else if (computerChose === 'paper' && playerChose === 'scissors') {
-				alert("User wins, Scissors cut paper. Reload and try again")
+function playRound() {
+	// Show a confirm dialogue allowing user to consent to play.
+	let playGame = confirm("Rock, Paper, or Scissors?");
+	// If playGame is accepted prompt for a choice.
+	if (playGame) {
+		// User fills playerChoice variable with their play
+		let playerChoice = prompt('Please enter Rock, Paper, or Scissors.');
+		if (playerChoice) {
+			// user trim function to change entry to lower case
+			let playerChose = playerChoice.trim().toLowerCase();
+			// check if playerChoice is any of the acceptable entries
+			console.log(playerChose)
+			if (playerChose === 'rock' || playerChose === 'paper' || playerChose === 'scissors') {
+				let computerChose = computerPlay();
+				console.log(computerChose);
+				// Find Tie Condition
+				if (computerChose === playerChose) {
+					alert("It's a tie!")
+					// Paper covers rock options
+				} else (computerChose != playerChose)
+				if (computerChose === 'paper' && playerChose === 'rock') {
+					alert("Computer wins, Paper covers rock.")
+				} else if (computerChose === 'rock' && playerChose === 'paper') {
+					alert("User wins, Paper Covers Rock.")
+					// Rock Breaks scissors options
+				} else if (computerChose === 'rock' && playerChose === 'scissors') {
+					alert("Computer wins, Rock breaks Scissors.")
+				} else if (computerChose === 'scissors' && playerChose === 'rock') {
+					alert("User wins, Rock breaks Scissors.")
+					// Scissors cut Paper Options
+				} else if (computerChose === 'scissors' && playerChose === 'paper') {
+					alert("Computer wins, Scissors cut paper.")
+				} else if (computerChose === 'paper' && playerChose === 'scissors') {
+					alert("User wins, Scissors cut paper.")
+				}
+			} else {
+				alert('Incorrect Entry Please enter Rock, Paper, or Scissors')
 			}
 		} else {
-			alert('Incorrect Entry Please enter Rock, Paper, or Scissors')
-			alert('Reload the page and start again')
+			alert("Sorry you changed your mind.")
 		}
 	} else {
-		alert("Sorry you changed your mind.")
+		alert("Ok, maybe next time.")
 	}
-} else {
-	alert("Ok, maybe next time.")
 }
+
+playRound()
+playRound()
+playRound()
+playRound()
+playRound()
+alert("Reload to Play Again")
