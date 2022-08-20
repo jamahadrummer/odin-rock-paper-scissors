d436ada814f103cf2e2045b62191c79bae61fcaf
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-20 14:19:47 -0500

    complete?

--- app.js
+++ app.js
@@ -2,53 +2,26 @@ function computerPlay() {
 	return computerArray[~~(Math.random() * computerArray.length)];
 }
 function playRound(playerChose, computerChose) {
-	if (playerChose === 'Rock' || playerChose === 'Paper' ||
-		playerChose === 'Scissors') {
-		// Find Tie Condition
-		if (computerChose === playerChose) {
-			// winDisp.classList.add('content');
-			// winDisp.textContent = "It's a Tie!";
-			// turnCont.appendChild(winDisp);
-			addPointComputer();
-			addPointUser();
-			// Paper covers Rock options
-		} else (computerChose != playerChose);
-		if (computerChose === 'Paper' && playerChose === 'Rock') {
-			// winDisp.classList.add('content');
-			// winDisp.textContent = 'Computer wins, Paper covers Rock.';
-			// turnCont.appendChild(winDisp);
-			addPointComputer();
-		} else if (computerChose === 'Rock' && playerChose === 'Paper') {
-			// winDisp.classList.add('content');
-			// winDisp.textContent = 'User wins, Paper Covers Rock.';
-			// turnCont.appendChild(winDisp);
-			addPointUser();
-			// Rock Breaks Scissors options
-		} else if (computerChose === 'Rock' && playerChose === 'Scissors') {
-			// winDisp.classList.add('content');
-			// winDisp.textContent = 'Computer wins, Rock breaks Scissors.';
-			// turnCont.appendChild(winDisp);
-			addPointComputer();
-		} else if (computerChose === 'Scissors' && playerChose === 'Rock') {
-			// winDisp.classList.add('content');
-			// winDisp.textContent = 'User wins, Rock breaks Scissors.';
-			// turnCont.appendChild(winDisp);
-			addPointUser();
-			// Scissors cut Paper Options
-		} else if (computerChose === 'Scissors' && playerChose === 'Paper') {
-			// winDisp.classList.add('content');
-			// winDisp.textContent = 'Computer wins, Scissors cut Paper.';
-			// turnCont.appendChild(winDisp);
-			addPointComputer();
-		} else if (computerChose === 'Paper' && playerChose === 'Scissors') {
-			// winDisp.classList.add('content');
-			// winDisp.textContent = 'User wins, Scissors cut Paper.';
-			// turnCont.appendChild(winDisp);
-			addPointUser();
-		}
+	// Find Tie Condition
+	if (computerChose === playerChose) {
+		// Paper covers Rock options
+	} else (computerChose != playerChose);
+	if (computerChose === 'Paper' && playerChose === 'Rock') {
+		addPointComputer();
+	} else if (computerChose === 'Rock' && playerChose === 'Paper') {
+		addPointUser();
+		// Rock Breaks Scissors options
+	} else if (computerChose === 'Rock' && playerChose === 'Scissors') {
+		addPointComputer();
+	} else if (computerChose === 'Scissors' && playerChose === 'Rock') {
+		addPointUser();
+		// Scissors cut Paper Options
+	} else if (computerChose === 'Scissors' && playerChose === 'Paper') {
+		addPointComputer();
+	} else if (computerChose === 'Paper' && playerChose === 'Scissors') {
+		addPointUser();
 	}
 }
-
 
 function addPointUser() {
 	userScore = userScore + 1;
@@ -56,6 +29,16 @@ function addPointUser() {
 
 function addPointComputer() {
 	computerScore = computerScore + 1;
+}
+
+function addText(insert, arr, container) {
+	insert.classList.add('content')
+	insert.textContent = arr.join('')
+	container.appendChild(insert);
+}
+
+function removeText(minus, container) {
+	container.removeChild(minus)
 }
 
 const turnCont = document.querySelector('.turnContainer')
@@ -66,28 +49,28 @@ const winDiv = document.createElement('div')
 const scoreDiv = document.createElement('div')
 const computerArray = ['Rock', 'Paper', 'Scissors'];
 const btns = document.querySelectorAll('.btn')
+const userWins = ['Game is over, User Wins!']
+const computerWins = ['Game is Over, Computer Wins']
 let userScore = 0;
 let computerScore = 0;
 
-
 btns.forEach(btn => {
 	btn.addEventListener('click', function () {
-		let computerChose = computerPlay();
-		let choseArr = ['User Chose ', btn.id, ', Computer Chose ', computerChose];
-		playRound(btn.id, computerChose);
-		//
-		turnDiv.classList.add('content')
-		turnDiv.textContent = choseArr.join('')
-		turnCont.appendChild(turnDiv);
-		// 
-		winDiv.classList.add('content')
-		winDiv.textContent = 'Win State'
-		winCont.appendChild(winDiv)
-		//
-		scoreDiv.classList.add('content')
-		scoreDiv.textContent = 'Score State'
-		scoreCont.appendChild(scoreDiv)
-		console.log(choseArr.join(''))
-	});
+		if (userScore < 5 && computerScore < 5) {
+			let computerChose = computerPlay();
+			playRound(btn.id, computerChose);
+			let choseArr = ['User Chose ', btn.id, ', Computer Chose ', computerChose];
+			let scoreArr = ['User: ', userScore, ' ', 'Computer: ', computerScore]
+			addText(turnDiv, choseArr, turnCont)
+			// addText(winDiv, winArr, winCont)
+			addText(scoreDiv, scoreArr, scoreCont)
+			console.log(choseArr.join(''))
+		} else if (userScore === 5) {
+			removeText(turnDiv, turnCont)
+			addText(winDiv, userWins, winCont)
+		} else if (computerScore === 5) {
+			removeText(turnDiv, turnCont)
+			addText(winDiv, computerWins, winCont)
+		}
+	})
 });
-
